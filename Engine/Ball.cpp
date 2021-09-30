e2e20cb69ef57df9506522a722f46049f5ce92c5
ArshesSH <53480645+ArshesSH@users.noreply.github.com>
2021-09-30 21:53:30 +0900

    Add Wall Collision

--- Engine/Ball.cpp
+++ Engine/Ball.cpp
@@ -17,3 +17,46 @@ void Ball::Update(float dt)
 {
 	pos += vel * dt;
 }
+
+bool Ball::DoWallcollision(const RectF& walls)
+{
+	bool isCollided = false;
+
+	const RectF ballRect = GetRect();
+	if (ballRect.left < walls.left)
+	{
+		ReboundX();
+		isCollided = true;
+	}
+	else if (ballRect.right > walls.right)
+	{
+		ReboundX();
+		isCollided = true;
+	}
+
+	if (ballRect.top > walls.top)
+	{
+		ReboundY();
+		isCollided = true;
+	}
+	else if (ballRect.bottom < walls.right)
+	{
+		ReboundY();
+		isCollided = true;
+	}
+}
+
+void Ball::ReboundX()
+{
+	vel.x = -vel.x;
+}
+
+void Ball::ReboundY()
+{
+	vel.y = -vel.y;
+}
+
+RectF Ball::GetRect() const
+{
+	return RectF::FromCenter(pos, radius, radius);
+}
