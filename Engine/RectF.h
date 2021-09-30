916ae1392cb3ce21aed32b7e38b1d23ae82db7f9
ArshesSH <53480645+ArshesSH@users.noreply.github.com>
2021-09-30 21:22:51 +0900

    Change File name Rect -> RectF

---
+++ Engine/RectF.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Vec2.h"
+
+class RectF
+{
+public:
+	RectF() = default;
+	RectF(float left_in, float right_in, float top_in, float bottom_in);
+	RectF(const Vec2& topLeft, const Vec2& bottomRight);
+	RectF(const Vec2& topLeft, float width, float height);
+
+	bool IsOverlapping(const RectF& other) const;
+	static RectF
+
+public:
+	float left;
+	float right;
+	float top;
+	float bottom;
+};
